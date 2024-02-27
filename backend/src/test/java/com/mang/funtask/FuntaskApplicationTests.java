9858cb5e1c3ec917c815dd1b2453c23ca8511fc4
Thiago Barros <bunbismuth@proton.me>
2024-02-27 14:33:44 -0300

    build: criação da parte backend do projeto

---
+++ backend/src/test/java/com/mang/funtask/FuntaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mang.funtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuntaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
