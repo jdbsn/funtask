b758ff8ac2e6eccdc11c9f102ed684072f91ba2c
Thiago Barros <bunbismuth@proton.me>
2024-02-29 19:48:41 -0300

    refactor: AtividadeDTO com descricao

--- backend/src/main/java/com/mang/funtask/dominio/dto/AtividadeDTO.java
+++ backend/src/main/java/com/mang/funtask/dominio/dto/AtividadeDTO.java
@@ -1,15 +1,15 @@
 package com.mang.funtask.dominio.dto;
 
-import com.mang.funtask.dominio.enums.FREQUENCIA;
+import com.mang.funtask.dominio.enums.Frequencia;
 import com.mang.funtask.dominio.modelos.Atividade;
 import com.mang.funtask.dominio.modelos.Crianca;
 
-public record AtividadeDTO(Double valorCredito, Double valorDebito, FREQUENCIA frequencia,
-                           Boolean foiConcluido, Crianca crianca) {
+public record AtividadeDTO(String descricao, Double valorCredito, Double valorDebito,
+                           Frequencia frequencia, Crianca crianca) {
 
   public AtividadeDTO(Atividade atividade) {
-    this(atividade.getValorCredito(), atividade.getValorDebito(), atividade.getFrequencia(),
-        atividade.getFoiConcluido(), atividade.getCrianca());
+    this(atividade.getDescricao(), atividade.getValorCredito(), atividade.getValorDebito(),
+        atividade.getFrequencia(), atividade.getCrianca());
   }
 
 }
