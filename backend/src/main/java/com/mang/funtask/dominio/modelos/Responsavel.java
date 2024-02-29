751e1fb13e6de49e8d226e48e4ccc178c49e441e
Thiago Barros <bunbismuth@proton.me>
2024-02-29 19:45:23 -0300

    refactor: adicionado lista de crianças

--- backend/src/main/java/com/mang/funtask/dominio/modelos/Responsavel.java
+++ backend/src/main/java/com/mang/funtask/dominio/modelos/Responsavel.java
@@ -1,36 +1,38 @@
 package com.mang.funtask.dominio.modelos;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.Set;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.util.UUID;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class Responsavel {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "id_responsavel")
-    private UUID id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "id_responsavel")
+  private UUID id;
 
-    @Column(nullable = false)
-    private String nome;
+  @Column(nullable = false)
+  private String nome;
 
-    @Column(nullable = false)
-    private String senha;
+  @Column(nullable = false)
+  private String senha;
 
-    @Column(nullable = false)
-    private String pin;
+  @Column(nullable = false)
+  private String pin;
 
-    public Responsavel() {
-
-    }
-
+  @OneToMany(mappedBy = "responsavel", cascade = CascadeType.ALL, orphanRemoval = true)
+  private Set<Crianca> criancas;
 }
