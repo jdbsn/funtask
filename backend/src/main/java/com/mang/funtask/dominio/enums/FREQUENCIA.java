fd93a4bd76820ddad5edbddcc30b3ba4fcfee621
Thiago Barros <bunbismuth@proton.me>
2024-02-29 19:48:10 -0300

    refactor: frequencia com annotations

--- backend/src/main/java/com/mang/funtask/dominio/enums/FREQUENCIA.java
+++ backend/src/main/java/com/mang/funtask/dominio/enums/FREQUENCIA.java
@@ -1,18 +1,15 @@
 package com.mang.funtask.dominio.enums;
 
-public enum FREQUENCIA {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Frequencia {
   DIARIA("Diaria"),
   SEMANAL("Semanal"),
   MENSAL("Mensal");
 
   private final String valor;
-
-  FREQUENCIA(String valor) {
-    this.valor = valor;
-  }
-
-  public String getValor() {
-    return valor;
-  }
 
 }
