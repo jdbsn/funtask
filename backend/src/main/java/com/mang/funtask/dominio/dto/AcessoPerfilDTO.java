384e49c09658272a7e68d30d2a1b93b9558a7c29
Thiago Barros <bunbismuth@proton.me>
2024-02-29 19:50:03 -0300

    refactor: correção de identação e espaçamento

--- backend/src/main/java/com/mang/funtask/dominio/dto/AcessoPerfilDTO.java
+++ backend/src/main/java/com/mang/funtask/dominio/dto/AcessoPerfilDTO.java
@@ -3,4 +3,5 @@ package com.mang.funtask.dominio.dto;
 import java.util.UUID;
 
 public record AcessoPerfilDTO(UUID id, int pin) {
+
 }
--- backend/src/main/java/com/mang/funtask/servicos/CriancaServico.java
+++ backend/src/main/java/com/mang/funtask/servicos/CriancaServico.java
@@ -3,23 +3,22 @@ package com.mang.funtask.servicos;
 import com.mang.funtask.dominio.dto.AcessoPerfilDTO;
 import com.mang.funtask.dominio.modelos.Crianca;
 import com.mang.funtask.repositorios.CriancaRepositorio;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class CriancaServico {
 
-    private CriancaRepositorio criancaRepo;
+  private CriancaRepositorio criancaRepo;
 
-    public Optional<Crianca> encontrarCrianca(AcessoPerfilDTO dto) {
-        return criancaRepo.findById(dto.id());
-    }
+  public Optional<Crianca> encontrarCrianca(AcessoPerfilDTO dto) {
+    return criancaRepo.findById(dto.id());
+  }
 
-    public boolean autorizarAcesso(Crianca crianca, int pin) {
-        return pin == crianca.getPin();
-    }
+  public boolean autorizarAcesso(Crianca crianca, int pin) {
+    return pin == crianca.getPin();
+  }
 
 }
