e078b99fe55f26cfe4cf8d5b8b0e050bb355a309
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-03-02 19:47:14 -0300

    fix: api url mapping

--- backend/src/main/java/com/mang/funtask/controladores/AtividadeController.java
+++ backend/src/main/java/com/mang/funtask/controladores/AtividadeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/responsavel/atividades")
+@RequestMapping("api/responsavel/atividades")
 public class AtividadeController {
 
   private final AtividadeServico atividadeServico;
