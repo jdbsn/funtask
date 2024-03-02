db48b043e699296debc96a2f525cd4b4798174c9
Thiago Barros <bunbismuth@proton.me>
2024-03-02 20:23:32 -0300

    feat: criado módulo atividade

---
+++ front/src/app/atividade/atividade.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ]
+})
+export class AtividadeModule { }
