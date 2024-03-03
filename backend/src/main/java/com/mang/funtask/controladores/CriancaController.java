4e2f40a7577a13e63588e6986aaf999e166ca76a
Thiago Barros <bunbismuth@proton.me>
2024-03-03 16:03:24 -0300

    refactor: padronização para Google Java Style

--- backend/src/main/java/com/mang/funtask/controladores/CriancaController.java
+++ backend/src/main/java/com/mang/funtask/controladores/CriancaController.java
@@ -2,6 +2,7 @@ package com.mang.funtask.controladores;
 
 import com.mang.funtask.dominio.dto.request.CriancaDTO;
 import com.mang.funtask.servicos.CriancaServico;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,24 +11,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 @RequestMapping("/api/crianca")
 @AllArgsConstructor
 public class CriancaController {
 
-    private CriancaServico criancaServico;
+  private CriancaServico criancaServico;
 
-    @PostMapping
-    public ResponseEntity<Map<String, String>> adicionarCrianca(@RequestBody CriancaDTO crianca) {
-        Map<String, String> mensagens = criancaServico.adicionarCrianca(crianca);
+  @PostMapping
+  public ResponseEntity<Map<String, String>> adicionarCrianca(@RequestBody CriancaDTO crianca) {
+    Map<String, String> mensagens = criancaServico.adicionarCrianca(crianca);
 
-        if(!mensagens.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(mensagens);
-        }
-
-        return ResponseEntity.status(HttpStatus.OK).build();
+    if (!mensagens.isEmpty()) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(mensagens);
     }
+
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
 
 }
