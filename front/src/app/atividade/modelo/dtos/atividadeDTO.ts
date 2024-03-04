11f946c477594102efecb449287e521a20e19d85
João Neto <jbsn1@discente.ifpe.edu.br>
2024-03-04 19:25:21 -0300

    fix: cadastro de atividades sem enviar id da crianca

--- front/src/app/atividade/modelo/dtos/atividadeDTO.ts
+++ front/src/app/atividade/modelo/dtos/atividadeDTO.ts
@@ -3,13 +3,13 @@ export class AtividadeDTO {
   valorCredito: number;
   valorDebito: number;
   frequencia: string;
-  criancaId: string;
+  idCrianca: string;
 
-  constructor(descricao: string, valorCredito: number, valorDebito: number, frequencia: string, criancaId: string) {
+  constructor(descricao: string, valorCredito: number, valorDebito: number, frequencia: string, idCrianca: string) {
     this.descricao = descricao;
     this.valorCredito = valorCredito;
     this.valorDebito = valorDebito;
     this.frequencia = frequencia;
-    this.criancaId = criancaId;
+    this.idCrianca = idCrianca;
   }
 }
